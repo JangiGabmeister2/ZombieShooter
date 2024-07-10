577aec4105fa7da565347f72fb87ec6ca9b45107
JangiGabmeister2 <109714078+JangiGabmeister2@users.noreply.github.com>
2024-07-10 15:46:56 +1000

    Zombie can move and Player can shoot
    Zombies will wait in idle until a player comes close. Then they will follow the player until they are too far to follow.
    Zombie move speed depends on whether they're walking or crawling.
    
    Given player ability to shoot gun. They can bring up their gun while moving.
    
    Added Cinemachine to track player while moving.

---
+++ Assets/Scripts/Zombies/ZombieBehaviour.cs
@@ -0,0 +1,141 @@
+using UnityEngine;
+
+public enum ZombieState
+{
+    Idle = 0,
+    Follow = 1,
+    Death = 2,
+}
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(Animator))]
+public class ZombieBehaviour : MonoBehaviour
+{
+    //zombie health
+    [SerializeField] private int _health = 3;
+    //the radius of the collider of the zombie where it checks whether or not the player is nearby
+    [SerializeField] private float _searchRadius = 5f;
+    //the speed at which the zombie moves (depends if walking or crawling)
+    [SerializeField] private float _moveSpeed = 5f;
+
+    //the current state of the zombie's behaviour
+    private ZombieState _state;
+    //reference to connected animator
+    private Animator _zombieAnimator;
+    //reference to the zombie's search collider
+    private SphereCollider _searchCollider;
+    //reference to the player transform
+    private Transform _player;
+
+    private bool _isMoving = false;
+
+    private void Start()
+    {
+        _zombieAnimator = GetComponent<Animator>();
+
+        _searchCollider = GetComponent<SphereCollider>();
+        _searchCollider.radius = _searchRadius;
+        _searchCollider.center = new Vector3(0, 1.8f, 0);
+        _searchCollider.isTrigger = true;
+    }
+
+    private void Update()
+    {
+        SwitchZombieState();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isMoving)
+        {
+            transform.position += transform.forward * _moveSpeed * Time.deltaTime;
+        }
+    }
+
+    #region Switch States
+    private void SwitchZombieState()
+    {
+        switch (_state)
+        {
+            case ZombieState.Idle:
+                IdleState();
+                break;
+            case ZombieState.Follow:
+                FollowState();
+                break;
+            case ZombieState.Death:
+                DeathState();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void IdleState()
+    {
+        _zombieAnimator.SetBool("isMoving", _isMoving);
+
+        //check whether the player is close by, if it is, follow player
+        if (_player != null)
+        {
+            _state = ZombieState.Follow;
+
+            _isMoving = true;
+        }
+
+        CheckHealth();
+    }
+
+    private void FollowState()
+    {
+        //rotates zombie transform to 'look' at the player
+        transform.LookAt(_player);
+
+        //plays zombie walk animation upon following of player
+        _zombieAnimator.SetBool("isMoving", _isMoving);
+
+        //check whether the player is too far to follow, if it is, go back to idle state
+        if (_player == null)
+        {
+            _state = ZombieState.Idle;
+
+            _isMoving = false;
+        }
+
+        CheckHealth();
+    }
+
+    private void DeathState()
+    {
+        //play animation for death
+        //but for not just disappear after a few secs
+
+        Destroy(gameObject, 2f);
+    }
+    #endregion
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _player = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _player = null;
+        }
+    }
+
+    private void CheckHealth()
+    {
+        //check health - if its 0 or less, zombie dies
+        if (_health <= 0)
+        {
+            _state = ZombieState.Death;
+        }
+    }
+}
